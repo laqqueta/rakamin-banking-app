37b4139e246efbf2b9c75949d07deb0eeea7a3ce
MrLuluCat <thenabil11@gmail.com>
2023-10-04 01:00:31 +0700

    resolve Dashboard conflict

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,28 +2,7 @@
 
 @section('page-title', 'Dashboard')
 @section('content')
-
-            <!-- Content Wrapper. Contains page content -->
-            <div class="content-wrapper">
-                <!-- Content Header (Page header) -->
-                <div class="content-header">
-                    <div class="container-fluid">
-                        <div class="row mb-2">
-                            <div class="col-sm-6">
-                                <h1 class="m-0">Dashboard</h1>
-                            </div><!-- /.col -->
-                            <div class="col-sm-6">
-                                <ol class="breadcrumb float-sm-right">
-                                    <li class="breadcrumb-item"><a href="">Home</a></li>
-                                    <li class="breadcrumb-item active">Dashboard</li>
-                                </ol>
-                            </div><!-- /.col -->
-                        </div><!-- /.row -->
-                    </div><!-- /.container-fluid -->
-                </div>
-            
-                <!-- /.content-header -->
-
+                         <!-- /.content-header -->
         <div class="jumbotron jumbotron-fluid">
     <div class="container">
         <div class="row">
