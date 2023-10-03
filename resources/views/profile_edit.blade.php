e71badace241762ba88e537090cff7c13d1b9dec
MrLuluCat <thenabil11@gmail.com>
2023-10-04 00:47:16 +0700

    Modified User Profile Front End

--- resources/views/profile_edit.blade.php
+++ resources/views/profile_edit.blade.php
@@ -2,21 +2,44 @@
 
 @section('content')
     <!-- Main -->
-    <div class="content-wrapper">
-        <h2>Profile</h2>
-        <div class="content-header">
-            <div class="container-fluid">
+    <div class="container mt-4">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <h2 class="text-center">Edit Profile</h2>
+                    </div>
+                    <div class="card-body">
     <form method="POST" action="{{ url('/profile-editsuccess/'.$edited->id) }}"  id='prfl-edit'> 
         @csrf
         @method('PUT')
-        Email :<br><input type="email" name="email" placeholder="E-mail" value='{{ $edited -> email }}' required readonly/> <br/><br/>
-        Nama :<br><input type="name" name="account_name" placeholder="Name" value='{{ $edited -> account_name }}' required /> <br/><br/>
-        Phone Number :<br><input type="number" name="phone_number" placeholder="Phone Number" value='{{ $edited -> phone_number }}' required /> <br/><br/>
-        Address:<br><textarea name="address" cols="40" rows="5" placeholder="account_adress" required>{{ $edited -> account_address }}</textarea><br><br>
-        <input type="submit" name="edit" value="Edit" id="edit" />
+
+        <div class="form-group">
+            <label for="account_name">Name</label>
+            <input type="text" name="account_name" id="account_name" class="form-control" value="{{ $edited->account_name }}" required>
+        </div>
+
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" class="form-control" value="{{ $edited->email }}" required readonly>
+        </div>
+
+        <div class="form-group">
+            <label for="phone_number">Phone Number</label>
+            <input type="tel" name="phone_number" id="phone_number" class="form-control" value="{{ $edited->phone_number }}" required>
+        </div>
+
+        <div class="form-group">
+            <label for="account_address">Address</label>
+            <textarea name="account_address" id="account_address" class="form-control" cols="40" rows="5" required>{{ $edited->account_address }}</textarea>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Save Changes</button>
     </form>
 
     </div>
         </div>
-            </div>
+    </div>
+    </div>
+    </div>
 @endsection
